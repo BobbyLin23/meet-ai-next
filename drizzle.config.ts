6cfb1e5bedd78c7647be41633546d4441b5ef8fd
BobbyLin <linzhangsheng23@gmail.com>
2025-06-04 19:52:53 +0800

    feat: add better-auth

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from 'drizzle-kit'
 config({ path: '.env.local' })
 
 export default defineConfig({
-  schema: './db/schema.ts',
-  out: './db/drizzle',
+  schema: './src/db/schemas.ts',
+  out: './src/db/drizzle',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
