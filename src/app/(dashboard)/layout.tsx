b3ece20c60b59e59ddbde3a6eb7ef9e10291baeb
BobbyLin <linzhangsheng23@gmail.com>
2025-06-06 19:39:31 +0800

    feat: add dashboard navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import { SidebarProvider } from '@/components/ui/sidebar'
 import { DashboardSidebar } from '@/feature/dashboard/components/dashboard-sidebar'
+import { DashboardNavbar } from '@/feature/dashboard/components/dashboard-navbar'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
+      <DashboardSidebar />
       <main className="bg-muted flex h-screen w-screen flex-col">
-        <DashboardSidebar />
+        <DashboardNavbar />
         {children}
       </main>
     </SidebarProvider>
---
+++ src/feature/dashboard/components/dashboard-command.tsx
@@ -0,0 +1,39 @@
+import { Dispatch, SetStateAction } from 'react'
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from '@/components/ui/command'
+
+export const DashboardCommand = ({
+  open,
+  setOpen,
+}: {
+  open: boolean
+  setOpen: Dispatch<SetStateAction<boolean>>
+}) => {
+  return (
+    <CommandDialog open={open} onOpenChange={setOpen}>
+      <CommandInput placeholder="Find a meeting or agent" />
+      <CommandList>
+        <CommandEmpty>No results found</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem value="calendar">Calendar</CommandItem>
+          <CommandItem value="search-emoji">Search Emoji</CommandItem>
+          <CommandItem value="calculator">Calculator</CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading="Settings">
+          <CommandItem value="profile">Profile</CommandItem>
+          <CommandItem value="billing">Billing</CommandItem>
+          <CommandItem value="settings">Settings</CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </CommandDialog>
+  )
+}
