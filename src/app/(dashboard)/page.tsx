2c7b1100304dd487feb2de7235d92b818cfe396e
BobbyLin <linzhangsheng23@gmail.com>
2025-06-06 23:12:03 +0800

    feat: add trpc

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,26 +1,5 @@
-'use client'
+import { HomeView } from '@/feature/dashboard/views/home-view'
 
-import { Button } from '@/components/ui/button'
-import { authClient } from '@/lib/auth-client'
-import { useRouter } from 'next/navigation'
-
-export default function Page() {
-  const router = useRouter()
-
-  async function handleLogout() {
-    await authClient.signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          router.push('/sign-in')
-        },
-      },
-    })
-  }
-
-  return (
-    <div className="bg-background p-3">
-      Hello World!
-      <Button onClick={handleLogout}>Log out</Button>
-    </div>
-  )
+export default async function Page() {
+  return <HomeView />
 }
