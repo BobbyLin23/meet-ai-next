02e37c4eec30c6460510f34f1eebcb684b50c860
BobbyLin <linzhangsheng23@gmail.com>
2025-06-07 16:07:45 +0800

    feat: add responsive-dialog

---
+++ src/components/responsive-dialog.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import React from 'react'
+
+import { useIsMobile } from '@/hooks/use-mobile'
+import {
+  Drawer,
+  DrawerContent,
+  DrawerTitle,
+  DrawerDescription,
+} from '@/components/ui/drawer'
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from '@/components/ui/dialog'
+
+interface ResponsiveDialogProps {
+  title: string
+  description: string
+  open: boolean
+  onOpenChange: (open: boolean) => void
+  children: React.ReactNode
+}
+
+export const ResponsiveDialog = ({
+  title,
+  description,
+  open,
+  onOpenChange,
+  children,
+}: ResponsiveDialogProps) => {
+  const isMobile = useIsMobile()
+
+  if (isMobile) {
+    return (
+      <Drawer open={open} onOpenChange={onOpenChange}>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{title}</DrawerTitle>
+            <DrawerDescription>{description}</DrawerDescription>
+          </DrawerHeader>
+          <div className="p-4">{children}</div>
+        </DrawerContent>
+      </Drawer>
+    )
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div className="p-4">{children}</div>
+      </DialogContent>
+    </Dialog>
+  )
+}
